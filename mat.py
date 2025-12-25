fa4f2f827f10c2bfd45118229b1826e3bb341980
sushma0918 <sushmamanapuram18@gmail.com>
2025-12-25 18:59:57 +0530

    Add files via upload

---
+++ mat.py
@@ -0,0 +1,14 @@
+#24331A05G8
+#1D ARRAY
+import numpy as np
+arr=np.array([10,15,20,30,35])
+print("array",arr)
+print("first element",arr[0])
+print("last element",arr[-1])
+print("slicing(index 1to3):",arr[1:4])
+print("shape:",arr.shape)
+print("size:",arr.size)
+print("datatype:",arr.dtype)
+print("minimum:",np.min(arr))
+print("maximum",np.max(arr))
+print("square of elements:",np.square(arr))
